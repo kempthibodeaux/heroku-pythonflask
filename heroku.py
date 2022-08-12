d4da287ef43e7ee74f386b1c4e4c69c1c7d321bf
Thibodeaux <kemp.thibodeaux@viasat.com>
2022-08-12 09:01:04 -0500

    initial commit

--- heroku.py
+++ heroku.py
@@ -14,4 +14,6 @@ def home():
     return "hello world"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='8000', debug=True)
+    app.secret_key = "ItIsASecret"
+    app.debug = True
+    app.run
