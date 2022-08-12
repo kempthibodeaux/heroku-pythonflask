58420af13f8d09d5d0373867e387ecf2e67d83b1
Thibodeaux <kemp.thibodeaux@viasat.com>
2022-08-12 07:18:08 -0500

    initial commit

---
+++ heroku.py
@@ -0,0 +1,17 @@
+
+from flask import Flask, flash, redirect, render_template, request, url_for
+import os
+from flask import send_from_directory
+
+app = Flask(__name__)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/favicon.jpg')
+@app.route('/')
+@app.route("/home")
+def home():
+    return "hello world"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='8000', debug=True)
